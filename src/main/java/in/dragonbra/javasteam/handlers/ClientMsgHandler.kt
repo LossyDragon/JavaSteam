08285961c087f9d3498aac9cf323cc5c5f8cb855
LossyDragon <thedragon1012@gmail.com>
2024-04-22 10:10:14 -0500

    Migrate ClientMsgHandler to kt

--- src/main/java/in/dragonbra/javasteam/handlers/ClientMsgHandler.kt
+++ src/main/java/in/dragonbra/javasteam/handlers/ClientMsgHandler.kt
@@ -1,55 +1,43 @@
-package in.dragonbra.javasteam.handlers;
+package `in`.dragonbra.javasteam.handlers
 
-import in.dragonbra.javasteam.base.IPacketMsg;
-import in.dragonbra.javasteam.steam.steamclient.SteamClient;
-import in.dragonbra.javasteam.steam.steamclient.callbacks.DisconnectedCallback;
+import `in`.dragonbra.javasteam.base.IPacketMsg
+import `in`.dragonbra.javasteam.steam.steamclient.SteamClient
+import `in`.dragonbra.javasteam.steam.steamclient.callbacks.DisconnectedCallback
 
 /**
  * This class implements the base requirements every message handler should inherit from.
  */
-@SuppressWarnings("unused")
-public abstract class ClientMsgHandler {
-
-    protected SteamClient client;
-
-    public void setup(SteamClient client) {
-        this.client = client;
-    }
+@Suppress("unused")
+abstract class ClientMsgHandler {
 
     /**
-     * Gets whether or not the related {@link SteamClient} should imminently expect the server to close the connection.
-     * If this is true when the connection is closed, the {@link DisconnectedCallback}'s
-     * {@link DisconnectedCallback#isUserInitiated()} property will be set to <b>true</b>.
+     * @return the underlying [SteamClient] for use in sending replies.
+     */
+    lateinit var client: SteamClient
+        protected set
+
+    /**
+     * Gets whether the related [SteamClient] should imminently expect the server to close the connection.
+     * If this is true when the connection is closed, the [DisconnectedCallback]'s
+     * [DisconnectedCallback.isUserInitiated] property will be set to **true**.
      *
-     * @return whether or not the related {@link SteamClient} should imminently expect the server to close the connection.
+     * @return whether the related [SteamClient] should imminently expect the server to close the connection.
      */
-    protected boolean isExpectDisconnection() {
-        return client.isExpectDisconnection();
-    }
-
-    /**
-     * Sets whether or not the related {@link SteamClient} should imminently expect the server to close the connection.
-     * If this is true when the connection is closed, the {@link DisconnectedCallback}'s
-     * {@link DisconnectedCallback#isUserInitiated()} property will be set to <b>true</b>.
-     *
-     * @param expectDisconnection whether or not the related {@link SteamClient} should imminently expect the server to close the connection.
-     */
-    @SuppressWarnings("SameParameterValue")
-    protected void setExpectDisconnection(boolean expectDisconnection) {
-        client.setExpectDisconnection(expectDisconnection);
-    }
-
-    /**
-     * @return the underlying {@link SteamClient} for use in sending replies.
-     */
-    public SteamClient getClient() {
-        return client;
-    }
+    @Suppress("SameParameterValue")
+    protected var isExpectDisconnection: Boolean
+        get() = client.isExpectDisconnection
+        protected set(expectDisconnection) {
+            client.isExpectDisconnection = expectDisconnection
+        }
 
     /**
      * Handles a client message. This should not be called directly.
      *
      * @param packetMsg The packet message that contains the data.
      */
-    public abstract void handleMsg(IPacketMsg packetMsg);
+    abstract fun handleMsg(packetMsg: IPacketMsg)
+
+    fun setup(client: SteamClient) {
+        this.client = client
+    }
 }
