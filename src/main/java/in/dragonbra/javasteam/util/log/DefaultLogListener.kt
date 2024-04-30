a4a67c94f57621024bb276c9d173dd8ba5c9f49f
LossyDragon <thedragon1012@gmail.com>
2024-04-30 15:26:02 -0500

    Migrate logger classes to kt

--- src/main/java/in/dragonbra/javasteam/util/log/DefaultLogListener.kt
+++ src/main/java/in/dragonbra/javasteam/util/log/DefaultLogListener.kt
@@ -1,52 +1,42 @@
-package in.dragonbra.javasteam.util.log;
+package `in`.dragonbra.javasteam.util.log
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.math.min
 
 /**
  * @author lngtr
  * @since 2018-03-02
  */
-public class DefaultLogListener implements LogListener {
+class DefaultLogListener : LogListener {
 
-    private static final SimpleDateFormat FORMAT = new SimpleDateFormat("HH:mm:ss.SSS");
-
-    @Override
-    public void onLog(Class<?> clazz, String message, Throwable throwable) {
-        if (clazz == null) {
-            throw new IllegalArgumentException("class is null");
-        }
-        String threadName = Thread.currentThread().getName();
-        threadName = threadName.substring(0, Math.min(10, threadName.length()));
-        String className = clazz.getName();
+    override fun onLog(clazz: Class<*>, message: String?, throwable: Throwable?) {
+        var threadName = Thread.currentThread().name
+        threadName = threadName.substring(0, min(10.0, threadName.length.toDouble()).toInt())
 
         if (message == null) {
-            System.out.printf("%s [%10s] %s%n", FORMAT.format(new Date()), threadName, className);
+            System.out.printf("%s [%10s] %s%n", FORMAT.format(Date()), threadName, clazz.name)
         } else {
-            System.out.printf("%s [%10s] %s - %s%n", FORMAT.format(new Date()), threadName, className, message);
+            System.out.printf("%s [%10s] %s - %s%n", FORMAT.format(Date()), threadName, clazz.name, message)
         }
 
-        if (throwable != null) {
-            throwable.printStackTrace();
-        }
+        throwable?.printStackTrace()
     }
 
-    public void onError(Class<?> clazz, String message, Throwable throwable) {
-        if (clazz == null) {
-            throw new IllegalArgumentException("class is null");
-        }
-        String threadName = Thread.currentThread().getName();
-        threadName = threadName.substring(0, Math.min(10, threadName.length()));
-        String className = clazz.getName();
+    override fun onError(clazz: Class<*>, message: String?, throwable: Throwable?) {
+        var threadName = Thread.currentThread().name
+        threadName = threadName.substring(0, min(10.0, threadName.length.toDouble()).toInt())
 
         if (message == null) {
-            System.err.printf("%s [%10s] %s%n", FORMAT.format(new Date()), threadName, className);
+            System.err.printf("%s [%10s] %s%n", FORMAT.format(Date()), threadName, clazz.name)
         } else {
-            System.err.printf("%s [%10s] %s - %s%n", FORMAT.format(new Date()), threadName, className, message);
+            System.err.printf("%s [%10s] %s - %s%n", FORMAT.format(Date()), threadName, clazz.name, message)
         }
 
-        if (throwable != null) {
-            throwable.printStackTrace();
-        }
+        throwable?.printStackTrace()
+    }
+
+    companion object {
+        private val FORMAT = SimpleDateFormat("HH:mm:ss.SSS")
     }
 }
--- src/main/java/in/dragonbra/javasteam/util/log/Logger.kt
+++ src/main/java/in/dragonbra/javasteam/util/log/Logger.kt
@@ -1,49 +1,30 @@
-package in.dragonbra.javasteam.util.log;
+package `in`.dragonbra.javasteam.util.log
 
 /**
  * @author lngtr
  * @since 2018-03-02
  */
-public class Logger {
+class Logger internal constructor(private val clazz: Class<*>) {
 
-    private final Class<?> clazz;
-
-    Logger(Class<?> clazz) {
-        if (clazz == null) {
-            throw new IllegalArgumentException("class is null");
-        }
-        this.clazz = clazz;
+    fun debug(throwable: Throwable) {
+        debug(null, throwable)
     }
 
-    public void debug(Throwable throwable) {
-        debug(null, throwable);
-    }
-
-    public void debug(String message) {
-        debug(message, null);
-    }
-
-    public void debug(String message, Throwable throwable) {
-        for (LogListener listener : LogManager.LOG_LISTENERS) {
-            if (listener != null) {
-                listener.onLog(clazz, message, throwable);
-            }
+    @JvmOverloads
+    fun debug(message: String?, throwable: Throwable? = null) {
+        LogManager.LOG_LISTENERS.forEach { listener ->
+            listener.onLog(clazz, message, throwable)
         }
     }
 
-    public void error(Throwable throwable) {
-        error(null, throwable);
+    fun error(throwable: Throwable) {
+        error(null, throwable)
     }
 
-    public void error(String message) {
-        error(message, null);
-    }
-
-    public void error(String message, Throwable throwable) {
-        for (LogListener listener : LogManager.LOG_LISTENERS) {
-            if (listener != null) {
-                listener.onError(clazz, message, throwable);
-            }
+    @JvmOverloads
+    fun error(message: String?, throwable: Throwable? = null) {
+        LogManager.LOG_LISTENERS.forEach { listener ->
+            listener.onError(clazz, message, throwable)
         }
     }
 }
