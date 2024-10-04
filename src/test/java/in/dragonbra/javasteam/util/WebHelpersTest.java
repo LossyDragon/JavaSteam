fdd429a5c573b87afdc718a623053c6101d95a96
LossyDragon <thedragon1012@gmail.com>
2024-10-03 23:50:01 -0500

    Update WebHelpers

--- src/test/java/in/dragonbra/javasteam/util/WebHelpersTest.java
+++ src/test/java/in/dragonbra/javasteam/util/WebHelpersTest.java
@@ -1,9 +1,8 @@
 package in.dragonbra.javasteam.util;
 
 import in.dragonbra.javasteam.TestBase;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author lngtr
@@ -12,8 +11,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class WebHelpersTest extends TestBase {
 
     @Test
-    public void urlEncode() {
+    public void isUrlSafeChar() {
+        // Safe
+        Assertions.assertTrue(WebHelpers.isUrlSafeChar('a'));
+        Assertions.assertTrue(WebHelpers.isUrlSafeChar('Z'));
+        Assertions.assertTrue(WebHelpers.isUrlSafeChar('0'));
+        Assertions.assertTrue(WebHelpers.isUrlSafeChar('-'));
+        Assertions.assertTrue(WebHelpers.isUrlSafeChar('_'));
+        // UnSafe
+        Assertions.assertFalse(WebHelpers.isUrlSafeChar(' '));
+        Assertions.assertFalse(WebHelpers.isUrlSafeChar('&'));
+        Assertions.assertFalse(WebHelpers.isUrlSafeChar('%'));
+        Assertions.assertFalse(WebHelpers.isUrlSafeChar('@'));
+    }
+
+    @Test
+    public void urlEncode_WithString() {
         String result = WebHelpers.urlEncode("encrypt THIS sTrInG1234 \10 \11 \12");
-        assertEquals("encrypt+THIS+sTrInG1234+%08+%09+%0A", result);
+        Assertions.assertEquals("encrypt+THIS+sTrInG1234+%08+%09+%0A", result);
+    }
+
+    @Test
+    public void urlEncode_WithByteArray() {
+        var input = "encrypt THIS sTrInG1234 \10 \11 \12".getBytes();
+        String result = WebHelpers.urlEncode(input);
+        Assertions.assertEquals("encrypt+THIS+sTrInG1234+%08+%09+%0A", result);
     }
 }
