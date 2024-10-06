3a802a38888f0e1eb26e3d28524ed08d8ce06948
LossyDragon <thedragon1012@gmail.com>
2024-10-06 17:44:59 -0500

    Simplify Event tests

--- src/test/java/in/dragonbra/javasteam/util/event/EventTest.java
+++ src/test/java/in/dragonbra/javasteam/util/event/EventTest.java
@@ -1,49 +1,47 @@
 package in.dragonbra.javasteam.util.event;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.mockito.Mockito.*;
-
-@SuppressWarnings("unchecked")
 class EventTest {
-    private Event<EventArgs> event;
-    private EventHandler<EventArgs> handler;
 
-    @BeforeEach
-    void setUp() {
-        event = new Event<>();
-        handler = mock(EventHandler.class);
+    static class TestEventHandler implements EventHandler<EventArgs> {
+        boolean eventHandled = false;
+        Object sender = null;
+        EventArgs eventArgs = null;
+
+        @Override
+        public void handleEvent(Object sender, EventArgs e) {
+            this.eventHandled = true;
+            this.sender = sender;
+            this.eventArgs = e;
+        }
     }
 
     @Test
-    void addEventHandler() {
+    void addEventHandlerAndHandleEvent() {
+        var event = new Event<>();
+        var handler = new TestEventHandler();
+
         event.addEventHandler(handler);
+
         event.handleEvent(this, EventArgs.EMPTY);
 
-        verify(handler, times(1)).handleEvent(this, EventArgs.EMPTY);
+        Assertions.assertTrue(handler.eventHandled);
+        Assertions.assertEquals(this, handler.sender);
+        Assertions.assertEquals(EventArgs.EMPTY, handler.eventArgs);
     }
 
     @Test
     void removeEventHandler() {
+        var event = new Event<>();
+        var handler = new TestEventHandler();
+
         event.addEventHandler(handler);
         event.removeEventHandler(handler);
 
         event.handleEvent(this, EventArgs.EMPTY);
 
-        verify(handler, never()).handleEvent(this, EventArgs.EMPTY);
-    }
-
-    @Test
-    void handleEventWithMultipleHandlers() {
-        EventHandler<EventArgs> anotherHandler = mock(EventHandler.class);
-
-        event.addEventHandler(handler);
-        event.addEventHandler(anotherHandler);
-
-        event.handleEvent(this, EventArgs.EMPTY);
-
-        verify(handler, times(1)).handleEvent(this, EventArgs.EMPTY);
-        verify(anotherHandler, times(1)).handleEvent(this, EventArgs.EMPTY);
+        Assertions.assertFalse(handler.eventHandled);
     }
 }
