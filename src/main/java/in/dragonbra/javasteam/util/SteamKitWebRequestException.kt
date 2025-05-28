8537418ebacc493510156fa08ed869b89237a48d
LossyDragon <thedragon1012@gmail.com>
2025-05-27 22:09:32 -0500

    Kotlinize SteamKitWebRequestException

--- src/main/java/in/dragonbra/javasteam/util/SteamKitWebRequestException.kt
+++ src/main/java/in/dragonbra/javasteam/util/SteamKitWebRequestException.kt
@@ -6,28 +6,21 @@ import java.lang.Exception
 
 /**
  * Thrown when an HTTP request fails.
+ *
+ * @constructor Initializes a new instance of the [SteamKitWebRequestException] class.
+ * @param message The message that describes the error.
+ * @param response HTTP response message including the status code and data.
  */
-@Suppress("MemberVisibilityCanBePrivate")
-class SteamKitWebRequestException(message: String) : Exception(message) {
+@Suppress("unused")
+class SteamKitWebRequestException(message: String, response: Response? = null) : Exception(message) {
 
     /**
      * Represents the status code of the HTTP response.
      */
-    var statusCode: Int = 0
+    val statusCode: Int = response?.code ?: 0
 
     /**
      * Represents the collection of HTTP response headers.
      */
-    var headers: Headers? = null
-        private set
-
-    /**
-     * Initializes a new instance of the [SteamKitWebRequestException] class.
-     * @param message The message that describes the error.
-     * @param response HTTP response message including the status code and data.
-     */
-    constructor(message: String, response: Response) : this(message) {
-        statusCode = response.code
-        headers = response.headers
-    }
+    val headers: Headers? = response?.headers
 }
